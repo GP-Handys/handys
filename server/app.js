a6ab40415dc996a8cbd5fedab938d5b52533da67
rayyanalhourani <76219100+rayyanalhourani@users.noreply.github.com>
2023-09-30 20:05:53 +0300

    added sequalize and user model

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require("express")
 require("dotenv").config()
+const connection = require('./database/database.js')
+require("./models/user.js")
 
 const app = express()
 
+connection.sync()
 app.listen(process.env.PORT)
---
+++ server/models/user.js
@@ -0,0 +1,39 @@
+const Sequelize = require('sequelize')
+const connection = require("../database/database")
+
+const user = connection.define("user",
+    {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        phone_number: {
+            type: Sequelize.INTEGER,
+            validator: {
+                is: /^(07[789]\d{7})$/,
+            }
+        },
+        is_sys_admin: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false
+        },
+        pfp_url: {
+            type: Sequelize.STRING
+        }
+    }
+)
+
+module.exports = user
