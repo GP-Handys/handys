a82db6b0b507c63dc34ef9de3d6027e265d43a94
Mohammad Khasawneh <mohdkh2010@live.com>
2023-09-30 22:38:39 +0300

    added post model

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express")
 require("dotenv").config()
 const connection = require('./database/database.js')
 require("./models/user.js")
+require("./models/post.js")
 
 const app = express()
 
---
+++ server/models/Post.js
@@ -0,0 +1,42 @@
+const Sequelize = require('sequelize')
+const connection = require("../database/database")
+const user = require("./user")
+
+const Post = connection.define("post", {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        content: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        img_url: {
+            type: Sequelize.STRING
+        },
+        votes: {
+            type: Sequelize.INTEGER,
+            defaultValue: 0
+        },
+        is_resolved: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        },
+        is_first_post: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false
+        }
+    }
+)
+
+// A user has many posts. A post belongs to many users.
+user.hasMany(Post)
+Post.belongsTo(user)
+
+// A post has many children posts. A child post belongs to one post. 
+Post.hasMany(Post, { as: 'ChildPosts', foreignKey: 'parentId' });
+Post.belongsTo(Post, { as: 'ParentPost', foreignKey: 'parentId' });
+
+module.exports = Post
