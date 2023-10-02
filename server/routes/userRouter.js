38cddf3b536da136466198da880432b999f57983
rayyanalhourani <76219100+rayyanalhourani@users.noreply.github.com>
2023-10-02 18:38:31 +0300

    User crud operations (#9)
    * add jwt decoder library
    
    * make default value for is_sys_admin
    
    * add user crud operations
    
    * edit status codes

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,18 +3,18 @@ require('dotenv').config()
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 const User = require("../models/User")
+const jwtDecode = require("jwt-decode");
 
 async function signup(req, res) {
     let { id, name, email, password, phone_number } = req.body;
     try {
-        const user = await User.findOne({ where: { email: email } })
+        let user = await User.findOne({ where: { email: email } })
         if (user) {
-            res.status(401).send("Invalid credentials")
+            res.status(400).send("User already exists")
         }
         else {
             let data = { id, name, email, password: await bcrypt.hash(password, 10), phone_number }
-            user = User.create(data)
-
+            user = await User.create(data)
             if (user) {
                 res.status(200).send("User created sucesfully")
             }
@@ -24,6 +24,7 @@ async function signup(req, res) {
         }
     }
     catch (error) {
+        console.log(error);
         res.status(500).send(error)
     }
 }
@@ -32,14 +33,15 @@ async function login(req, res) {
     let { email, password } = req.body
     try {
         let user = await User.findOne({ where: { email: email } })
-        
-        if (user.email=="") {
+
+        if (user == null) {
             res.status(401).send("Invalid credentials")
         }
         else {
             let isCorrectPassword = await bcrypt.compare(password, user.password)
             if (isCorrectPassword) {
-                let token = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+                let id = user.id
+                let token = jwt.sign({ id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
                 res.send(token)
             }
             else {
@@ -48,8 +50,79 @@ async function login(req, res) {
         }
     }
     catch (error) {
-        res.Status(500).send(error)
+        res.status(500).send(error)
     }
 }
 
-module.exports = { login, signup}
+async function getUser(req, res) {
+    let id = req.params.id
+    let user = await User.findOne({ where: { id: id } })
+
+    if (user) {
+        res.status(200).send(user)
+    } else {
+        res.sendStatus(404)
+    }
+}
+
+async function editUser(req, res) {
+    try {
+        
+        const decodedtoken = jwtDecode(req.get("Authorization"));
+        let path_id = req.params.id
+        let user = await User.findOne({ where: { id: path_id } })
+        let { id, name, email, password, phone_number } = req.body;
+        let data = { id, name, email, password: await bcrypt.hash(password, 10), phone_number }
+
+        if(user==null){
+            res.sendStatus(404)
+            return;
+        }
+        
+        if (user.is_sys_admin || decodedtoken.id == user.id) {
+            user = await user.update(data)
+            if (user) {
+                res.status(200).send("user modified")
+            }
+            else {
+                res.sendStatus(500)
+            }
+        }
+        else {
+            res.sendStatus(403)
+        }
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
+
+async function deleteUser(req, res) {
+    try {
+        const decodedtoken = jwtDecode(req.get("Authorization"));
+        let id = req.params.id
+        let token_userid = decodedtoken.id;
+        let user = await User.findOne({ where: { id: id } })
+
+        if(user==null){
+            res.sendStatus(404)
+            return;
+        }
+
+        if (user.is_sys_admin || id == token_userid) {
+            
+            let user = await User.destroy({ where: { id: id } })
+            if (user) {
+                res.status(200).send("User deleted")
+            }
+            else {
+                res.status(500).send("Failed to delete user")
+            }
+        } else {
+            res.sendStatus(401)
+        }
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
+
+module.exports = { login, signup, getUser, editUser, deleteUser }
