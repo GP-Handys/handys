19ac3522e0aa1b33070477393fc4fe9ce16a2ba3
Mohammad Khasawneh <77096504+mhmdKhasawneh@users.noreply.github.com>
2023-10-02 20:40:28 +0300

    added review models for both shops and items (#11)

---
+++ server/models/ShopReview.js
@@ -0,0 +1,32 @@
+const Sequelize = require("sequelize");
+const connection = require("../database/database");
+const Shop = require("./Shop")
+const User = require("./User")
+
+const ShopReview = connection.define('shop_reviews', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    content: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    stars: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validator: {
+            min: 1,
+            max: 5
+        }
+    }
+})
+
+ShopReview.belongsTo(Shop)
+Shop.hasMany(ShopReview)
+
+ShopReview.belongsTo(User)
+User.hasMany(ShopReview)
+
+module.exports = ShopReview
