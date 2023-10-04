5c59b67454a510fdb5d623a2c65593a18f3ec8a0
Mohammad Khasawneh <mohdkh2010@live.com>
2023-10-04 23:38:50 +0300

    added order router :D

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const { tokenValidate } = require("../middleware/authMiddleware")
 const userRouter = require("./userRouter")
+const orderRouter = require("./orderRouter")
 
 
 //user Routes
@@ -10,5 +11,10 @@ router.post("/api/users/signup", userRouter.signup)
 router.get("/api/users/getUser/:id", tokenValidate, userRouter.getUser)
 router.put("/api/users/editUser/:id", tokenValidate, userRouter.editUser)
 router.delete("/api/users/deleteUser/:id", tokenValidate, userRouter.deleteUser)
+
+//order routes
+router.post("/api/orders/place", orderRouter.placeOrder)
+router.get("/api/orders/shop/:shopId", orderRouter.getOrderForShopId)
+router.get("/api/orders/user/:userId", orderRouter.getOrderForUserId)
 
 module.exports = router
---
+++ server/routes/orderRouter.js
@@ -0,0 +1,78 @@
+const Order = require("../models/Order")
+const Item = require("../models/Item")
+
+module.exports.placeOrder = async (req, res) => {
+    try {
+        const {
+            delivery_address,
+            payment_method,
+            price,
+            is_confirmed,
+            userId,
+            shopId,
+            items
+        } = req.body;
+
+        const order = await Order.create({
+            deliver_address: delivery_address,
+            payment_method: payment_method,
+            price: price,
+            is_confirmed: is_confirmed,
+            userId: userId,
+            shopId: shopId,
+        });
+
+        if (items && items.length > 0) {
+            for (const itemData in items) {
+                const { itemId, quantity } = itemData;
+
+                const item = await Item.findByPk(itemId);
+
+                if (!item) {
+                    return res.status(400).json({ error: `Item with ID ${itemId} not found` });
+                }
+
+                // add to item_orders joint table
+                await order.addItem(item, { through: { quantity } });
+            }
+        }
+
+        // 201 = resource created. better convention :)
+        res.status(201).json(order);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error })
+    }
+}
+
+module.exports.getOrderForShopId = async (req, res) => {
+    const shopId = req.params.shopId
+
+    try {
+        const orders = Order.findAll({
+            where: {
+                shopId: shopId
+            }
+        })
+
+        res.status(200).json(orders)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: error })
+    }
+}
+
+module.exports.getOrderForUserId = async (req, res) => {
+    const userId = req.params.userId
+
+    try {
+        const orders = Order.findAll({
+            userId: userId
+        })
+
+        res.status(200).json(orders)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: error })
+    }
+}
