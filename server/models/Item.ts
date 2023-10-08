09b60fd4cc4ea07cfd9573792232b8514dd430f8
Laith otaibi <78775064+LaithOt@users.noreply.github.com>
2023-10-08 22:09:15 +0300

    updated item.ts

--- server/models/Item.ts
+++ server/models/Item.ts
@@ -1,63 +1,88 @@
-const Sequelize = require("sequelize");
-const connection = require("../database/database");
-const Shop = require("./Shop")
+import { DataTypes, Model } from 'sequelize';
+import {connection} from '../database/database';
 
-const Item = connection.define("item", {
-   id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-   },
-   name: {
-      type: Sequelize.STRING,
-      allowNull: false,
-   },
-   description: {
-      type: Sequelize.STRING,
-      allowNull: false,
-   },
-   base_price: {
-      type: Sequelize.FLOAT,
-      allowNull: false,
-      validate: {
-         isFloat: true,
-         min: 0,
+import {Shop} from './Shop'
+
+
+class Item extends Model{
+   public id! : number;
+   public name! : string;
+   public description! : string;
+   public base_price! : number;
+   public discount! : number;
+   public rating! : number;
+   public quantity! : number;
+   public is_customizable! : boolean;
+   public img_url! : string;
+   public is_deleted! : boolean;
+
+}
+
+Item.init(
+   {
+    
+      id: {
+         type: DataTypes.INTEGER,
+         primaryKey: true,
+         autoIncrement: true,
+      },
+      name: {
+         type: DataTypes.STRING,
+         allowNull: false,
+      },
+      description: {
+         type: DataTypes.STRING,
+         allowNull: false,
+      },
+      base_price: {
+         type: DataTypes.FLOAT,
+         allowNull: false,
+         validate: {
+            isFloat: true,
+            min: 0,
+         },
+      },
+      discount: {
+         type: DataTypes.FLOAT,
+         allowNull: true,
+         validate: {
+            isFloat: true,
+            min: 0,
+            max: 100, // percentage
+         },
+      },
+      rating: {
+         type: DataTypes.INTEGER,
+         defaultValue: 1,
+         validate: {
+            min: 1,
+            max: 5,
+         },
+      },
+      quantity: {
+         type: DataTypes.INTEGER,
+         allowNull: false
+      },
+      is_customizable: {
+         type: DataTypes.BOOLEAN,
+         defaultValue: false
+      },
+      img_url: {
+         type: DataTypes.STRING,
+         allowNull: true,
+      },
+      is_deleted: {
+         type: DataTypes.BOOLEAN,
+         defaultValue: false
       },
    },
-   discount: {
-      type: Sequelize.FLOAT,
-      allowNull: true,
-      validate: {
-         isFloat: true,
-         min: 0,
-         max: 100, // percentage
-      },
-   },
-   rating: {
-      type: Sequelize.INTEGER,
-      defaultValue: 1,
-      validate: {
-         min: 1,
-         max: 5,
-      },
-   },
-   quantity: {
-      type: Sequelize.INTEGER,
-      allowNull: false
-   },
-   is_customizable: {
-      type: Sequelize.BOOLEAN,
-      defaultValue: false
-   },
-   img_url: {
-      type: Sequelize.STRING,
-      allowNull: true,
-   },
-   is_deleted: {
-      type: Sequelize.BOOLEAN,
-      defaultValue: false
+
+   {
+      sequelize: connection,
+      modelName: 'Item'
    }
-})
+
+);
 
 Item.belongsTo(Shop)
 Shop.hasMany(Item)
