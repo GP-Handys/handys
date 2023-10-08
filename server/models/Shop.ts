e0a44c0165021fd7642b2dcc4e7a135c6f2b5a99
Mohammad Khasawneh <77096504+mhmdKhasawneh@users.noreply.github.com>
2023-10-08 22:00:19 +0300

    migrated post and shop model to ts (#21)

--- server/models/Shop.ts
+++ server/models/Shop.ts
@@ -1,54 +1,73 @@
-const Sequelize = require("sequelize");
-const connection = require("../database/database");
-const User = require("./User");
+import { DataTypes, Model } from 'sequelize';
+import { connection } from '../database/database'
+import { User } from './user'
 
-
-const Shop = connection.define("shop", {
-   id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-   },
-   name: {
-      type:Sequelize.STRING,
-      allowNull: false
-   },
-   rating: {
-      type: Sequelize.INTEGER,
-      defaultValue: 1,
-      validate: {
+class Shop extends Model {
+   public id!: number;
+   public name!: string;
+   public rating!: number;
+   public is_premium!: boolean;
+   public is_deleted!: boolean;
+   public is_approved!: boolean;
+   public pfp_url?: string | null;
+   public bio!: string;
+   public socialMediaLink!: string;
+   public readonly createdAt!: Date;
+   public readonly updatedAt!: Date;
+ }
+ 
+Shop.init(
+   {
+     id: {
+       type: DataTypes.INTEGER,
+       primaryKey: true,
+       autoIncrement: true,
+     },
+     name: {
+       type: DataTypes.STRING,
+       allowNull: false,
+     },
+     rating: {
+       type: DataTypes.INTEGER,
+       defaultValue: 1,
+       validate: {
          min: 1,
          max: 5,
-      },
+       },
+     },
+     is_premium: {
+       type: DataTypes.BOOLEAN,
+       allowNull: false,
+     },
+     is_deleted: {
+       type: DataTypes.BOOLEAN,
+       allowNull: false,
+     },
+     is_approved: {
+       type: DataTypes.BOOLEAN,
+       defaultValue: false,
+     },
+     pfp_url: {
+       type: DataTypes.STRING,
+       allowNull: true,
+     },
+     bio: {
+       type: DataTypes.STRING,
+       allowNull: false,
+     },
+     socialMediaLink: {
+       type: DataTypes.STRING,
+       allowNull: false,
+       validate: {
+         isUrl: true,
+       },
+     },
    },
-   is_premium: {
-      type: Sequelize.BOOLEAN,
-      allowNull: false,
-   },
-   is_deleted: {
-      type: Sequelize.BOOLEAN,
-      allowNull: false,
-   },
-   is_approved: {
-      type:Sequelize.BOOLEAN,
-      defaultValue: false
-   },
-   pfp_url: {
-      type: Sequelize.STRING,
-      allowNull: true
-   },
-   bio: {
-      type: Sequelize.STRING,
-      allowNull: false,
-   },
-   socialMediaLink: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      validate: {
-         isUrl: true
-      }
+   {
+     sequelize: connection,
+     modelName: 'Shop',
    }
-})
+ );
 
 Shop.belongsTo(User)
 User.hasMany(Shop)
