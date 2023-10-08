d9e88a4bb714c5e896d229cdb4bbbf1c9feec766
rayyanalhourani <76219100+rayyanalhourani@users.noreply.github.com>
2023-10-08 22:19:31 +0300

    change to ts (#24)

--- server/models/Order.ts
+++ server/models/Order.ts
@@ -1,48 +1,71 @@
-const Sequelize = require('sequelize')
-const connection = require("../database/database")
-const User = require('./User')
-const Item = require('./Item')
-const Shop = require("./Shop")
+import { DataTypes, Model, Sequelize } from "sequelize";
+import { connection } from "../database/database";
+import { User } from "./User";
+import { Item } from "./Item";
+import { Shop } from "./Shop";
 
-const Order = connection.define("order", {
-      id: {
-         type: Sequelize.INTEGER,
-         primaryKey: true,
-         autoIncrement: true,
-      },
-      delivery_address: {
-         type : Sequelize.STRING,
-         allowNull: false,
-      },
-      payment_method: {
-         type: Sequelize.STRING,
-         allowNull: false
-      },
-      price: {
-         type: Sequelize.DOUBLE,
-         allowNull: false
-      },
-      is_confirmed: {
-         type: Sequelize.BOOLEAN,
-         defaultValue: false
-      }
-   }
-)
+class Order extends Model {
+  public id!: number;
+  public delivery_address!: string;
+  public payment_method!: string;
+  public price!: number;
+  public is_confirmed!: boolean;
+}
 
-const ItemOrder = connection.define("item_orders", {
-   quantity: {
-     type: Sequelize.INTEGER,
-     allowNull: false,
-   }
- });
+Order.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    delivery_address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    payment_method: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DOUBLE,
+      allowNull: false,
+    },
+    is_confirmed: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  {
+    sequelize: connection,
+    modelName: "order",
+  }
+);
 
-Order.belongsTo(Shop)
-Shop.hasMany(Order)
+class ItemOrder extends Model {
+  public quantity!: number;
+}
 
-Order.belongsTo(User)
-User.hasMany(Order)
+ItemOrder.init(
+  {
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: connection,
+    modelName: "item_orders",
+  }
+);
 
-Item.belongsToMany(Order, {through: "item_orders"})
-Order.belongsToMany(Item, {through: "item_orders"})
+Order.belongsTo(Shop);
+Shop.hasMany(Order);
 
-export {Order}
+Order.belongsTo(User);
+User.hasMany(Order);
+
+Item.belongsToMany(Order, { through: ItemOrder, as: "orders" });
+Order.belongsToMany(Item, { through: ItemOrder, as: "items" });
+
+export { Order, ItemOrder };
