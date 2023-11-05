d0a8633d3f8e074f3f7954267339834b6dcfa103
Mohammad Khasawneh <mohdkh2010@live.com>
2023-11-05 23:22:52 +0300

    added db port

--- server/database/database.ts
+++ server/database/database.ts
@@ -6,9 +6,11 @@ const DATABASE_NAME = process.env.DATABASE_NAME as string;
 const HOST = process.env.HOST as string;
 const DATABASE_USERNAME = process.env.DATABASE_USERNAME as string;
 const PASSWORD = process.env.PASSWORD as string;
+const DB_PORT = Number(process.env.DB_PORT);
 
 const connection = new Sequelize(DATABASE_NAME, DATABASE_USERNAME, PASSWORD, {
   host: HOST,
+  port: DB_PORT,
   dialect: "mysql",
 });
 
