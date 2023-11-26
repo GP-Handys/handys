d55c28dda24e8f26bf9a6c9ed2f8b2220a8dd5de
Mohammad Khasawneh <77096504+mhmdKhasawneh@users.noreply.github.com>
2023-11-26 21:21:15 +0300

    firebase (#48)
    * npm i firebase
    
    * added firebase config

---
+++ client/storage/firebase.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCmk9cv1iklQ7PoePNE9fMM1t2livZciMw",
+  authDomain: "handys-1.firebaseapp.com",
+  projectId: "handys-1",
+  storageBucket: "handys-1.appspot.com",
+  messagingSenderId: "878121021435",
+  appId: "1:878121021435:web:c7ca438db714383c5ce55e"
+};
