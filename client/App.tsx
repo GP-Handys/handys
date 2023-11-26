9275d25933efea6206a48f10bdf8a89ce15f712d
Mohammad Khasawneh <mohdkh2010@live.com>
2023-11-26 21:20:37 +0300

    added firebase config

--- client/App.tsx
+++ client/App.tsx
@@ -2,25 +2,29 @@ import { NavigationContainer } from "@react-navigation/native";
 import TabBarNavigationContainer from "./components/navigation/TabBarNavigationContainer";
 import OnboardingNavigationContainer from "./components/navigation/OnboardingNavigationContainer";
 import { Stack } from "./components/navigation/NavigationStack";
+import { firebaseConfig } from "./storage/firebase";
+import { initializeApp } from "firebase/app";
 
 export default function App() {
+  initializeApp(firebaseConfig);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="OnboardingScreensContainer">
         <Stack.Screen
-        name="OnboardingScreensContainer"
-        component={OnboardingNavigationContainer}
-        options={{
-          headerShown: false,
-        }}
+          name="OnboardingScreensContainer"
+          component={OnboardingNavigationContainer}
+          options={{
+            headerShown: false,
+          }}
         />
         <Stack.Screen
-        name="MainScreensContainer"
-        component={TabBarNavigationContainer}
-        options={{
-          headerShown: false,
-          gestureEnabled: false
-        }}
+          name="MainScreensContainer"
+          component={TabBarNavigationContainer}
+          options={{
+            headerShown: false,
+            gestureEnabled: false,
+          }}
         />
       </Stack.Navigator>
     </NavigationContainer>
