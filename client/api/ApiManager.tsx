69e0e29769437e1f9c7e222f8c84df8c1cf86c72
LaithOt <laith.otaibi@gmail.com>
2023-12-03 18:01:33 +0300

    removed duplicate file

--- client/api/ApiManager.tsx
+++ client/api/ApiManager.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = "https://1fd8-109-107-253-237.ngrok-free.app";
+const BACKEND_URL = "https://9ba3-91-186-226-150.ngrok-free.app";
 const ApiManager = axios.create({
   baseURL: `${BACKEND_URL}/api`,
   responseType: "json",
