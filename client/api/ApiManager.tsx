ccef8d2f241edac3ff752e634474575b36087d72
AssemMusallam <136684286+AssemMusallam@users.noreply.github.com>
2023-12-12 13:58:02 +0300

    removed components for support

--- client/api/ApiManager.tsx
+++ client/api/ApiManager.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = "https://5e63-176-29-227-114.ngrok-free.app";
+const BACKEND_URL = "https://6086-91-186-248-2.ngrok-free.app";
 const ApiManager = axios.create({
   baseURL: `${BACKEND_URL}/api`,
   responseType: "json",
