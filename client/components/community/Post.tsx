3d14591c7245731777c24abea9f3d966cd9d29c3
LaithOt <laith.otaibi@gmail.com>
2023-12-16 17:41:17 +0300

    formatting

--- client/components/community/Post.tsx
+++ client/components/community/Post.tsx
@@ -1,16 +1,15 @@
-import { View, StyleSheet, Text, Image } from "react-native";
+import { View, StyleSheet, Text, Image, Dimensions } from "react-native";
 import PostOwnerHeader from "../../components/community/PostOwnerHeader";
 import TimeStamp from "../../components/community/TimeStamp";
 import LikeButton from "../../components/community/LikeButton";
 import CommentButton from "../../components/community/CommentButton";
 import { PostModel } from "../../models/Post";
-import { Dimensions } from "react-native";
 
 interface PostProps {
   post: PostModel;
 }
 
-export default function Post({ post }: PostProps) {
+export default function Post({ post }: Readonly<PostProps>) {
   return (
     <View>
       <View style={styles.userProfile}>
@@ -25,18 +24,17 @@ export default function Post({ post }: PostProps) {
         <View style={styles.mainPost}>
           <Text style={{ color: "white", fontSize: 15 }}>{post.content}</Text>
           {post.img_url && (
-              <Image style={styles.postImg} source={{ uri: post.img_url }} />
+            <Image style={styles.postImg} source={{ uri: post.img_url }} />
           )}
           <View style={styles.footer}>
-              <LikeButton />
-              <CommentButton />
+            <LikeButton />
+            <CommentButton />
           </View>
         </View>
       </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   userProfile: {
     flexDirection: "row",
@@ -53,17 +51,16 @@ const styles = StyleSheet.create({
   },
   postImg: {
     marginTop: 12,
-    width: Dimensions.get('window').width-20,
-    height:400,
-    resizeMode:"contain",
+    width: Dimensions.get("window").width - 20,
+    height: 400,
+    resizeMode: "contain",
   },
   footer: {
     flexDirection: "row",
-    alignItems:"baseline",
-    marginTop:15,
-    gap:20,
-    marginLeft:10
-    
+    alignItems: "baseline",
+    marginTop: 15,
+    gap: 20,
+    marginLeft: 10,
   },
   comment: {
     marginLeft: 40,
--- client/components/community/PostModal.tsx
+++ client/components/community/PostModal.tsx
@@ -16,7 +16,6 @@ import CustomTextInput from "../CustomTextInput";
 import pickImageAndStore from "../../storage/store";
 import { addPost } from "../../api/CommunityApi";
 import { getProfile } from "../../api/UserApi";
-import { Dimensions } from "react-native";
 
 interface PostModalProps {
   isVisible: boolean;
@@ -53,27 +52,27 @@ export default function PostModal({
         <CommonBackgroundWithSafeArea>
           <View style={{ backgroundColor: COLORS.commonBackground }}>
             <View style={styles.ModalHeader}>
-            
-                <TouchableOpacity
-                  onPress={onDismiss}
-                  style={styles.CloseButton}
-                >
-                  <MaterialIcons name="close" size={23} color="white" />
-                </TouchableOpacity>
-              
-                <Text style={styles.HeaderText}> Create post</Text>
-              
-                <TouchableOpacity
-                  onPress={handleAddPost}
-                  style={[styles.PostButton, { opacity: postText ? 1 : 0.5 ,position:"absolute",right:0},]}
-                  disabled={!postText}
-                >
-                  <Text style={styles.InnerPostButton}>Post</Text>
-                </TouchableOpacity>
+              <TouchableOpacity onPress={onDismiss} style={styles.CloseButton}>
+                <MaterialIcons name="close" size={23} color="white" />
+              </TouchableOpacity>
+              <Text style={styles.HeaderText}> Create post</Text>
+              <TouchableOpacity
+                onPress={handleAddPost}
+                style={[
+                  styles.PostButton,
+                  {
+                    opacity: postText ? 1 : 0.5,
+                    position: "absolute",
+                    right: 0,
+                  },
+                ]}
+                disabled={!postText}
+              >
+                <Text style={styles.InnerPostButton}>Post</Text>
+              </TouchableOpacity>
             </View>
             <ThematicBreak />
           </View>
-
           <View
             style={{
               flexDirection: "row",
@@ -84,7 +83,6 @@ export default function PostModal({
             }}
           >
             <PostOwnerHeader userId={user.id} />
-
             <TouchableOpacity>
               <MaterialIcons
                 name="insert-photo"
@@ -102,7 +100,6 @@ export default function PostModal({
               />
             </TouchableOpacity>
           </View>
-
           <View style={{ marginTop: 5, paddingLeft: 10 }}>
             <CustomTextInput
               placeholder={"Share your thoughts!"}
@@ -113,7 +110,6 @@ export default function PostModal({
               }}
             />
           </View>
-
           {postImgPicked && (
             <TouchableOpacity
               onPress={async () => {
@@ -136,15 +132,15 @@ export default function PostModal({
 }
 const styles = StyleSheet.create({
   ModalHeader: {
-    justifyContent:"center",
+    justifyContent: "center",
     alignItems: "center",
     marginBottom: 15,
     marginHorizontal: 15,
   },
   CloseButton: {
     width: 30,
-    position:"absolute",
-    left:0
+    position: "absolute",
+    left: 0,
   },
   HeaderText: {
     fontSize: 20,
@@ -169,6 +165,6 @@ const styles = StyleSheet.create({
     width: 350,
     height: 250,
     resizeMode: "contain",
-    alignSelf:"center"
+    alignSelf: "center",
   },
 });
