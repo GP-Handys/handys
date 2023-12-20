10c16bce93137e51d8aa70bcd3f7a587e6c47b48
rayyanalhourani <76219100+rayyanalhourani@users.noreply.github.com>
2023-12-20 18:43:30 +0300

    edit search shop

--- client/api/ApiManager.tsx
+++ client/api/ApiManager.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = "https://e832-176-29-171-210.ngrok-free.app";
+const BACKEND_URL = "https://1914-91-186-240-62.ngrok-free.app";
 const ApiManager = axios.create({
   baseURL: `${BACKEND_URL}/api`,
   responseType: "json",
