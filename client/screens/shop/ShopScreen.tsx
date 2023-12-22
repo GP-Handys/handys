e4bbce8ceaf655110105f9cede3801646aa4be48
Mohammad Khasawneh <mohdkh2010@live.com>
2023-12-22 20:44:17 +0300

    shop screen wip

--- client/screens/shop/ShopScreen.tsx
+++ client/screens/shop/ShopScreen.tsx
@@ -13,15 +13,53 @@ import {
 import COLORS from "../../common/colors";
 import { useNavigation } from "@react-navigation/native";
 import { StackProps } from "../../components/navigation/NavigationStack";
+import { Item } from "../../models/Item";
+import { useEffect, useState } from "react";
+import { Shop } from "../../models/Shop";
+import { getItemsForShopId } from "../../api/ItemApi";
+import { getShopById } from "../../api/ShopApi";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getProfile } from "../../api/UserApi";
+import { StarRatingDisplay } from "react-native-star-rating-widget";
+import { FontAwesome5 } from "@expo/vector-icons";
+import ThematicBreak from "../../components/ThematicBreak";
 
 export default function ShopScreen({ route }: any) {
+  const [items, setItems] = useState<Item[]>([]);
+  const [shop, setShop] = useState<any>();
+  const [userId, setUserId] = useState<number>();
   const navigation = useNavigation<StackProps["navigation"]>();
   const { shopId } = route.params;
+
+  const fetchItemsForShopId = async () => {
+    await getItemsForShopId(shopId).then((res) => {
+      if (res.status === 200) {
+        setItems(res.data);
+      }
+    });
+  };
+  const fetchShopDataById = async () => {
+    await getShopById(shopId).then((res) => {
+      if (res.status === 200) {
+        setShop(res.data);
+      }
+    });
+  };
+  const getProfileByToken = async () => {
+    await getProfile().then((res) => {
+      setUserId(res.id);
+    });
+  };
+  useEffect(() => {
+    fetchItemsForShopId();
+    fetchShopDataById();
+    getProfileByToken();
+  }, []);
+
   return (
     <CommonScrollableBackground>
-      <CommonBackgroundWithSafeArea>
-        <View style={styles.shopHeader}>
-          <Text style={styles.shopNameHeader}>Crafter's Store</Text>
+      {shop?.userId == userId && (
+        <View style={styles.addItemsButtonContainer}>
           <TouchableOpacity
             style={styles.addItemButton}
             onPress={() => {
@@ -33,13 +71,41 @@ export default function ShopScreen({ route }: any) {
             <Text style={{ fontSize: 16, fontWeight: "500" }}>Add item</Text>
           </TouchableOpacity>
         </View>
-      </CommonBackgroundWithSafeArea>
-      <Image
-        source={require("../../assets/crafter.png")}
-        style={styles.shopImage}
-      />
-      <View></View>
-      {/* <FlatList/> */}
+      )}
+      <Image source={{ uri: shop?.pfp_url }} style={styles.shopImage} />
+      <View style={styles.footerContainer}>
+        <View>
+          <Text style={styles.footerShopName}>{shop?.name}</Text>
+          <StarRatingDisplay
+            rating={shop?.rating}
+            starSize={16}
+            color={"white"}
+            starStyle={{ width: 2 }}
+            style={styles.rating}
+          />
+        </View>
+        {shop?.userId == userId && (
+          <View>
+            <View style={styles.subToPremiumContainer}>
+              <TouchableOpacity
+                style={styles.subToPremiumButton}
+                onPress={() => {
+                  navigation.navigate("AddItemScreen", {
+                    shopId: shopId,
+                  });
+                }}
+              >
+                <FontAwesome5 name="crown" size={22} color="black" />
+                <Text style={{ fontSize: 14, fontWeight: "bold" }}>
+                  Get premium
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+      </View>
+      <Text style={styles.bio}>{shop?.bio}</Text>
+      <ThematicBreak />
     </CommonScrollableBackground>
   );
 }
@@ -51,23 +117,57 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     marginBottom: 25,
   },
-  shopNameHeader: {
-    fontSize: 24,
-    color: "white",
-    fontWeight: "500",
-    marginLeft: 15,
+  addItemsButtonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    marginRight: 15,
   },
   addItemButton: {
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: COLORS.CTAButtonBackground,
-    marginRight: 20,
-    paddingHorizontal: 15,
     height: 37,
+    width: 100,
     borderRadius: 6,
+    marginVertical: 10,
   },
   shopImage: {
     width: "100%",
-    height: 200,
+    aspectRatio: 3 / 2,
+  },
+  footerContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 10,
+  },
+  footerShopName: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: 25,
+    color: "white",
+  },
+  rating: {
+    marginLeft: 19,
+  },
+  subToPremiumContainer: {
+    marginRight: 15,
+  },
+  subToPremiumButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: COLORS.CTAButtonBackground,
+    height: 37,
+    width: 135,
+    borderRadius: 6,
+    paddingHorizontal: 10,
+  },
+  bio: {
+    fontSize: 14,
+    color: "white",
+    opacity: 0.5,
+    marginLeft: 15,
+    marginVertical: 17,
   },
 });
