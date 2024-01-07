796945f6483c0e93fddc6e4a3dddd1b30cc71092
Mohammad Khasawneh <mohdkh2010@live.com>
2024-01-07 22:41:26 +0300

    backend refactoring

--- server/routes/imageGenRouter.ts
+++ server/routes/imageGenRouter.ts
@@ -3,11 +3,20 @@ import { openai } from "../app";
 
 export const generateImage = async (req: Request, res: Response) => {
   const prompt: any = req.query.prompt;
-  const response = await openai.images.generate({
-    model: "dall-e-2",
-    prompt: prompt,
-    n: 1,
-    size: "1024x1024",
-  });
-  res.status(200).json(response.data[0].url);
+  try {
+    const response = await openai.images.generate({
+      model: "dall-e-2",
+      prompt: prompt,
+      n: 1,
+      size: "1024x1024",
+    });
+
+    if (response.data && response.data[0] && response.data[0].url) {
+      res.status(200).json(response.data[0].url);
+    } else {
+      res.status(500).json({ error: "Error generating image" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error generating image" });
+  }
 };
