931d7cd6fa6b503bf1da6ff1cf20b485a743b5d6
Mohammad Khasawneh <mohdkh2010@live.com>
2024-01-08 22:48:56 +0300

    created history screen

---
+++ client/screens/ai/History.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { CommonScrollableBackground} from "../../common/background";
+
+export default function GenerationHistory() {
+    return (
+        <CommonScrollableBackground>
+            <Text>History</Text>
+        </CommonScrollableBackground>
+    );
+}
