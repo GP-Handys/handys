65f012123d89646100bfd8db85cec9213e39149f
Mohammad Khasawneh <mohdkh2010@live.com>
2024-01-14 14:27:51 +0300

    fix screen

--- client/App.tsx
+++ client/App.tsx
@@ -5,7 +5,6 @@ import { Stack } from "./components/navigation/NavigationStack";
 import { firebaseConfig } from "./storage/firebase";
 import { initializeApp } from "firebase/app";
 import SendTicketScreen from "./screens/support/SendTicket";
-import Home from "./screens/mainscreens/home";
 import DoneScreen from "./screens/support/Done";
 import COLORS from "./common/colors";
 import CreateShop from "./screens/shop/CreateShop";
@@ -27,6 +26,7 @@ import AiPromptScreen from "./screens/ai/Prompt";
 import GeneratedImageScreen from "./screens/ai/GeneratedImage";
 import ShopContactScreen from "./screens/shop/Contact";
 import { ItemOrderScreen } from "./screens/profile/ItemOrders";
+import { MyOrders } from "./screens/profile/MyOrders";
 
 export default function App() {
   initializeApp(firebaseConfig);
@@ -283,7 +283,18 @@ export default function App() {
             headerBackTitleVisible: false,
           }}
         />
-
+        <Stack.Screen
+          name="MyOrdersScreen"
+          component={MyOrders}
+          options={{
+            title: "My Orders",
+            headerTitleStyle: { color: "white" },
+            headerStyle: { backgroundColor: COLORS.commonBackground },
+            headerTintColor: "white",
+            headerTitleAlign: "center",
+            headerBackTitleVisible: false,
+          }}
+        />
         <Stack.Screen
           name="ItemOrderScreen"
           component={ItemOrderScreen}
