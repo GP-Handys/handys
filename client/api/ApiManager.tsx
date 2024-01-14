a08a9b9030230624ff0ef16c00d5b76ca3f30d18
LaithOt <78775064+LaithOt@users.noreply.github.com>
2024-01-14 15:03:02 +0300

    xd

--- client/api/ApiManager.tsx
+++ client/api/ApiManager.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = "https://0f8a-109-107-243-20.ngrok-free.app";
+const BACKEND_URL = "https://5727-2a01-9700-10b6-3600-809d-82c0-6918-9ca4.ngrok-free.app";
 const ApiManager = axios.create({
   baseURL: `${BACKEND_URL}/api`,
   responseType: "json",
