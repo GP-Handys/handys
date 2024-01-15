2103673d2d995338122a55a1997b31c950e2d917
rayyanalhourani <76219100+rayyanalhourani@users.noreply.github.com>
2024-01-15 13:57:24 +0300

    disable button

--- client/helpers/checkout/checkoutHelper.ts
+++ client/helpers/checkout/checkoutHelper.ts
@@ -4,7 +4,7 @@ import { StackParamList } from "../../components/navigation/NavigationStack";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { confirmOrder } from "../../api/OrderApi";
 
-export default function checkoutHelper(
+export default async function checkoutHelper(
   street_name: string,
   apt_number: string,
   floor: string,
@@ -12,22 +12,28 @@ export default function checkoutHelper(
   price: number,
   buildingNumber:string,
   instructions:string,
-  navigation: NativeStackNavigationProp<StackParamList>
+  navigation: NativeStackNavigationProp<StackParamList>,
+  setDisableClick:any
 ) {
   let phoneReg = STRINGS.phoneValidator;
 
   if (!phoneReg.test(String(phone_number))) {
     Alert.alert(STRINGS.failPopUp, "You have to enter valid phone number");
+    setDisableClick(false)
   } else if (street_name.length == 0) {
     Alert.alert(STRINGS.failPopUp, "You have to enter street name");
+    setDisableClick(false)
   } else if (floor.length == 0) {
     Alert.alert(STRINGS.failPopUp, "You have to enter the floor");
+    setDisableClick(false)
   } else if (buildingNumber.length == 0) {
     Alert.alert(STRINGS.failPopUp, "You have to enter the building number");
+    setDisableClick(false)
   } else if (apt_number.length == 0) {
     Alert.alert(STRINGS.failPopUp, "You have to enter apartment number");
+    setDisableClick(false)
   } else {
-    confirmOrder({ street_name, apt_number, floor, phone_number, price,buildingNumber,instructions });
+    await confirmOrder({ street_name, apt_number, floor, phone_number, price,buildingNumber,instructions });
     navigation.navigate("DonePlaceOrder");
   }
 }
