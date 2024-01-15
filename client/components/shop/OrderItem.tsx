694d04c5fb3c95bd30199a9126deec81c3abe801
Mohammad Khasawneh <mohdkh2010@live.com>
2024-01-15 20:57:42 +0300

    wip

---
+++ client/components/shop/OrderItem.tsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from "react";
+import { View, Image, Text, StyleSheet } from "react-native";
+import { Item } from "../../models/Item";
+import { getItemById } from "../../api/ItemApi";
+import { ItemOrderModel } from "../../models/ItemOrder";
+
+interface Props {
+  itemOrder: ItemOrderModel;
+}
+
+export default function OrderItem({ itemOrder }: Props) {
+  const [itemRating, setItemRating] = useState<number>(0);
+  const [item, setItem] = useState<Item>();
+
+  const fetchItem = async () => {
+    await getItemById(itemOrder.itemId).then((res) => {
+      if (res.status === 200) {
+        setItem(res.data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    fetchItem();
+  }, []);
+
+  return (
+    <>
+      {item && (
+        <View style={{ marginHorizontal: 15 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginBottom: 20,
+            }}
+          >
+            <View style={{ flex: 1 }}>
+              <Text style={styles.itemName}>{item?.name}</Text>
+              <View style={{ marginRight: 5, marginTop: 5, marginBottom: 5 }}>
+                <Text style={{ opacity: 0.25, color: "white" }}>
+                  Customization:
+                  {itemOrder.customization.length === 0
+                    ? " This item is not customizable."
+                    : " " + itemOrder.customization}
+                </Text>
+              </View>
+            </View>
+            <View>
+              <Image
+                source={{ uri: item?.img_url ?? "" }}
+                style={styles.itemImage}
+              />
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text
+                  style={{ fontSize: 13, color: "white", fontWeight: "bold" }}
+                >
+                  x{itemOrder.quantity}
+                </Text>
+                <Text style={styles.itemPrice}>
+                  JOD {item?.base_price * itemOrder.quantity}
+                </Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      )}
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  footerContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 10,
+  },
+  itemImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 8,
+  },
+  itemName: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  itemPrice: {
+    color: "white",
+    fontSize: 13,
+    fontWeight: "bold",
+  },
+});
